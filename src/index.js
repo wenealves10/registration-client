4ebe6dd71a67ef7430405ec425999c1868e4b208
Wene Alves de Oliveira <weneplay5@gmail.com>
2020-12-27 12:07:07 -0300

    index main

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-import './assets/css/styles.css'
+import './assets/css/styles.css';
+import ValidatePassword from './modules/ ValidatePassword';
+import Sign from './modules/Sign';
+import Login from './modules/Login';
+
+
+let btnSign = document.querySelector('#btnSign');
+let btnLogin = document.querySelector('#btnLogin');
+
+if (btnSign) {
+    btnSign.addEventListener('click', function (event) {
+        const validatePassword = ValidatePassword('#passwordUser', '#passwordConfirm');
+        Sign('#nameUser', '#emailUser', validatePassword);
+    });
+}
+if (btnLogin) {
+
+    btnLogin.addEventListener('click', function (event) {
+        Login('#emailLogin', '#passwordLogin');
+        alert('Reload');
+    });
+}
