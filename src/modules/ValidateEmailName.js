e69f05434dcf6ed6baaed43798f623de88b7c918
Wene Alves de Oliveira <weneplay5@gmail.com>
2020-12-27 12:04:02 -0300

    validate of email and name

---
+++ src/modules/ValidateEmailName.js
@@ -0,0 +1,18 @@
+export function ValidateEmail(Email) {
+    let email = document.querySelector(Email).value;
+    let val = new RegExp(/^[A-Za-z0-9_\-\.]+@[A-Za-z0-9_\-\.]{2,}\.[A-Za-z0-9]{2,}(\.[A-Za-z0-9])?/);
+   if(email != '' && val.test(email)){
+       return email;
+   }else{
+       return false;
+   }
+}
+
+export function ValidateName(Name){
+    let name = document.querySelector(Name).value;
+    if(name != '' && name != ' ' && name != undefined && isNaN(name)){
+        return name;
+    }else{
+        return false;
+    }
+}
